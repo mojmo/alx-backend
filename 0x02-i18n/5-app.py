29320c81a162dcfaa91cf2578d3d1d15edfc9ab0
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-02 15:11:39 +0300

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+"""
+A simple Flask application with basic internationalization
+(i18n) configuration.
+"""
+
+from typing import Dict, Union
+from flask import Flask, render_template, request
+import flask
+from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """The configuration class for the Flask application."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello_world() -> str:
+    """Renders the '5-index.html' template and returns the response.
+
+    This function is the main entry point for the web application. It
+    handles the root URL ('/') and returns the rendered output of the
+    '5-index.html' template.
+
+    Returns:
+        str: The rendered HTML content from the template.
+    """
+    return render_template('5-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Returns the locale from the request.
+
+    Returns:
+        str: The locale from the request.
+    """
+
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+def get_user() -> Union[Dict[str, Union[str, None]], None]:
+    """Returns the user from the request.
+
+    Returns:
+        str: The user from the request.
+    """
+
+    user = int(request.args.get('login_as'))
+
+    if user and user in users:
+        return users[user]
+
+    return None
+
+
+@app.before_request
+def before_request():
+    """Sets the locale and timezone for the request."""
+
+    user = get_user()
+
+    if user:
+        flask.g.user = user
+
+
+if __name__ == '__main__':
+    app.run()
